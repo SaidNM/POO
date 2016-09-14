40864bf913b6b5b41ba7fe09498ea8fedaa38a89
POO <jajaja@gmail.com>
2016-09-14 13:32:51 -0500

    Falta fecha

--- Coffee/src/pruebas/Main.java
+++ Coffee/src/pruebas/Main.java
@@ -1,9 +1,21 @@
 package pruebas;
 
 import fuentes.order;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {   
+        Date fecha=new Date();
+        SimpleDateFormat ft=new SimpleDateFormat("MM dd,YYYY");
+        
+        System.out.println(ft.format(fecha));
+        
+        
+        
+        
+        
         order miorden=new order(32);
         System.out.println(miorden);
         
